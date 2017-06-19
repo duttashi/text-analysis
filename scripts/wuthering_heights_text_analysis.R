63d23bb00f8afe4a9f6cea3d14640f1426088fc4
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-19 17:29:53 +0800

    add code for creating a custom theme

--- scripts/wuthering_heights_text_analysis.R
+++ scripts/wuthering_heights_text_analysis.R
@@ -9,6 +9,7 @@ library(gutenbergr)
 library(dplyr)
 library(magrittr)
 library(tidytext)
+library(ggplot2)
 
 # find the gutenberg title for analysis. Look for the "gutenberg_id"
 gutenberg_works(title == "Wuthering Heights")
@@ -21,7 +22,6 @@ head(wuthering_heights)
 # Notice, it has two columns, the first column contains "gutenberg_id" and the second col contains the novel text.
 # Do not remove the first column even though it contains a single repeating value 768, because, this column will be used subsequently for filtering data
 
-
 # Data preprocessing
 
 ## Step 1: To create a tidy dataset, we need to restructure it in the one-token-per-row format, which as we saw earlier is done with the unnest_tokens() function.
@@ -30,18 +30,27 @@ tidy_novel_data<- wuthering_heights %>%
   anti_join(stop_words, by="word") 
 
 # use dplyr’s count() to find the most common words in the novel
+
 tidy_novel_data %>%
   count(word, sort=TRUE)
 
-str(tidy_novel_data)
-library(ggplot2)
+# Lets create a custom theme
+mytheme<- theme_bw()+
+  theme(plot.title = element_text(color = "darkred"))+
+  theme(panel.border = element_rect(color = "steelblue", size = 2))+
+  theme(plot.title = element_text(hjust = 0.5)) # where 0.5 is to center
 
+# From the count of common words from above code, now plotting the most common words
 tidy_novel_data %>%
   count(word, sort = TRUE) %>%
-  filter(n > 100 & n<151) %>%
+  filter(n >100  & n<400) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word, n)) +
   geom_col() +
-  xlab(NULL) +
-  coord_flip()
+  #xlab(NULL) +
+  coord_flip()+
+  mytheme+
+  ggtitle("Top words in Wuthering Heights")
+
+
 
