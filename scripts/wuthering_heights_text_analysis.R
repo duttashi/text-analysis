7fbf2e554de9b4fecf3f0dd50f3d1818146f8ead
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-19 10:31:17 +0800

    added code for initial text data preprocessing as well as visualization

--- scripts/wuthering_heights_text_analysis.R
+++ scripts/wuthering_heights_text_analysis.R
@@ -1,6 +1,14 @@
+# clean the workspace environment
+# Reference: http://tidytextmining.com/tidytext.html
+# Reference: https://cran.r-project.org/web/packages/tidytext/vignettes/tidytext.html
+
+rm(list = ls())
+
+# Load the required libraries
 library(gutenbergr)
 library(dplyr)
 library(magrittr)
+library(tidytext)
 
 # find the gutenberg title for analysis. Look for the "gutenberg_id"
 gutenberg_works(title == "Wuthering Heights")
@@ -8,16 +16,32 @@ gutenberg_works(title == "Wuthering Heights")
 # Once the id is found, pass it as a value to function "gutenberg_download() as shown below
 wuthering_heights <- gutenberg_download(768)
 
-# Look at the data structure
+# Look at the data structure, its a tibble
 head(wuthering_heights)
 # Notice, it has two columns, the first column contains "gutenberg_id" and the second col contains the novel text.
-# extracting the novel text column and saving it as a data frame
-text_data<-as.data.frame(wuthering_heights[,2])
-typeof(text_data)
-str(text_data)
-text_data
+# Do not remove the first column even though it contains a single repeating value 768, because, this column will be used subsequently for filtering data
 
-# Reference: http://tidytextmining.com/tidytext.html
-library(tidytext)
-text_data %>%
-  unnest_tokens(word, text)
+
+# Data preprocessing
+
+## Step 1: To create a tidy dataset, we need to restructure it in the one-token-per-row format, which as we saw earlier is done with the unnest_tokens() function.
+tidy_novel_data<- wuthering_heights %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words, by="word") 
+
+# use dplyr’s count() to find the most common words in the novel
+tidy_novel_data %>%
+  count(word, sort=TRUE)
+
+str(tidy_novel_data)
+library(ggplot2)
+
+tidy_novel_data %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 100 & n<151) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
