948546f1837847349349b78bf5dd74e99ea3dd6b
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-28 09:53:31 +0800

    basic text analysis and plotting completed

--- scripts/wuthering_heights_sentiment_analysis.R
+++ scripts/wuthering_heights_sentiment_analysis.R
@@ -4,6 +4,15 @@
 library(tidyverse)
 library(tidytext)
 library(stringi)
+library(SentimentAnalysis)
+
+# Analyze sentiment
+sentiment<- analyzeSentiment(dtm, language = "english",
+                             removeStopwords = TRUE)
+# Extract dictionary-based sentiment according to the QDAP dictionary
+sentiment$SentimentQDAP
+# View sentiment direction (i.e. positive, neutral and negative)
+convertToDirection(sentiment$SentimentQDAP)
 
 # The three general-purpose lexicons available in tidytext package are
 # 
@@ -13,3 +22,31 @@ library(stringi)
 
 # I will use Bing lexicon which is simply a tibble with words and positive and negative words
 get_sentiments("bing") %>% head
+
+wuthering_heights %>%
+  # split text into words
+  unnest_tokens(word, text) %>%
+  # remove stop words
+  anti_join(stop_words, by = "word") %>%
+  # add sentiment scores to words
+  left_join(get_sentiments("bing"), by = "word") %>%
+  # count number of negative and positive words
+  count(word, sentiment) %>%
+  spread(key = sentiment, value = n) %>%
+  #ungroup %>%
+  # create centered score
+  mutate(sentiment = positive - negative - 
+           mean(positive - negative)) %>%
+  # select_if(sentiment) %>%
+  # reorder word levels
+  #mutate(word = factor(as.character(word), levels = levels(word)[61:1])) %>%
+  # plot
+  ggplot(aes(x = word, y = sentiment)) + 
+  geom_bar(stat = "identity", aes(fill = word)) + 
+  theme_classic() + 
+  theme(axis.text.x = element_text(angle = 90)) + 
+  coord_flip() + 
+  #ylim(0, 200) +
+  coord_cartesian(ylim=c(0,40)) +  # Explain ggplot2 warning: “Removed k rows containing missing values”
+  ggtitle("Centered sentiment scores", 
+          subtitle = "for Wuthering Heights")
