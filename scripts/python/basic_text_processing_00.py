dd9a200bc4e9bd795e9c4e5bf8f9b1dfa08bbc37
ashish <ashish.dutt8@gmail.com>
2020-04-10 08:36:23 +0800

    initial commit

---
+++ scripts/python/basic_text_processing_00.py
@@ -0,0 +1,51 @@
+# Objective #1: To read multiple text files in a given directory & write content of each text file to a list
+# Objective #2: To search for a given text string in each file and write its line number to a dictionary and print it
+
+import re, os, string
+from collections import Counter
+
+fileData = [] # initialize an empty list
+stripped = []
+phrase = [] # initialize an empty list
+searchPhrase = 'beauty'
+
+# the path to text files
+filePath = "C:\\Users\\Ashoo\\Documents\\PythonPlayground\\text-analysis\\data\\plainText"
+for filename in os.listdir(filePath): 
+    # search for files ending with .txt extension and read them in memory
+    if filename.strip().endswith('.txt'):
+        print(filename)
+        with(open(os.path.join(filePath,filename),'rt')) as f:
+            try:
+                # read all text with newlines included and save data to variable
+                fileTxt = f.read() 
+                f.close()
+                
+                # basic text cleaning process
+                words = re.split(r'\W+', fileTxt)
+                words = [word.lower() for word in words]
+                table = str.maketrans('', '', string.punctuation)
+                
+                # concatenating parsed data
+                stripped += [w.translate(table) for w in words]   
+                
+                # search for a given string in clean text. If found then print the line number
+                for num, line in enumerate(stripped,1):
+                    if(searchPhrase in line):
+                        print(searchPhrase, " found at line number: ",num)
+            
+            # if file not found, log an error        
+            except IOError as e:
+                logging.error('Operation failed: {}' .format(e.strerror))
+                sys.exit(0)
+                
+        # create a dictionary that stores the unique words found in the text files 
+        countlist = Counter(stripped) 
+print("Count of uniique words in text files", filename, " are: ")
+
+# print the unique word and its frequency of occurence
+for key, val in countlist.items():
+    print(key, val)
+
+# print end of program statement
+print("End")
