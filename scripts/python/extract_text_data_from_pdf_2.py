202541bb9f563ebe5221309977b6f0ad80dabcc8
Ashish Dutt <ashish.dutt8@gmail.com>
2021-06-27 11:53:01 +0530

    modified script
    - add new code including documentation.

--- scripts/python/extract_text_data_from_pdf_2.py
+++ scripts/python/extract_text_data_from_pdf_2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jun 25 09:12:31 2021
-
+Pdf file data downloaded from: https://ncrb.gov.in/en/crime-in-india-table-addtional-table-and-chapter-contents
 @author: Ashish
 """
 
@@ -11,12 +11,28 @@ import os
 cur_path = os.path.dirname(__file__)
 print(cur_path)
 
-# get data
-new_path = os.path.relpath('..\\data\\pdf\\file1.pdf', cur_path)
+# set path for loading pdf file in memory
+new_path = os.path.relpath('..\\..\\data\\pdf\\pdf_tbl1.pdf', cur_path)
 
+# read pdf, extract data
 with pp.open(new_path) as pdf:
     page = pdf.pages[0]
     text=page.extract_text()
     print(text)
-    
+
+# set table settings in pdf file
+table_settings = {
+    "vertical_strategy": "lines",
+    "horizontal_strategy": "text",
+    "intersection_x_tolerance": 15
+    }
+
+# read pdf file, extract table data
+with pp.open(new_path) as pdf:
+    mypage = pdf.pages[0]
+    tbls = mypage.find_tables()
+    print("Tables: ", tbls)
+    tbl_content = tbls[0].extract(x_tolerance=5)
+    print("Table content: ",tbl_content)
+
 
